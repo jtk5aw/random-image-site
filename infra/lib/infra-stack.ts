1f5cd78d305083c684ef094ddb68f57d9064ce22
Jackson Kennedy <jtk5aw@virginia.edu>
2023-01-07 00:42:53 -0500

    Add site and default frontend
    Includes both CDK changes and adding the react app itself

--- infra/lib/infra-stack.ts
+++ infra/lib/infra-stack.ts
@@ -1,9 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as lambda from "aws-cdk-lib/aws-lambda";
-import * as s3 from "aws-cdk-lib/aws-s3";
 import { PolicyStatement } from 'aws-cdk-lib/aws-iam';
+import * as lambda from 'aws-cdk-lib/aws-lambda';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
+import * as targets from 'aws-cdk-lib/aws-route53-targets';
 import * as apiGateway from 'aws-cdk-lib/aws-apigateway';
+import * as route53 from 'aws-cdk-lib/aws-route53';
+import * as acm from 'aws-cdk-lib/aws-certificatemanager';
+import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 
 export class InfraStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -13,9 +18,10 @@ export class InfraStack extends cdk.Stack {
     const bucket_name = 'get-image-lambda-bucket';
     const table_name = 'image-info-table';
     const table_primary_key = 'id';
+    const web_app_domain = 'jtken.com';
     
     // Storage resources
-    const bucket = new s3.Bucket(this, "TestBucket", {
+    const bucket = new s3.Bucket(this, 'TestBucket', {
       bucketName: bucket_name,
       publicReadAccess: false,
     });
@@ -69,6 +75,61 @@ export class InfraStack extends cdk.Stack {
 
     const todaysImage = randomImageApi.root.addResource('todays-image');
     todaysImage.addMethod('GET', new apiGateway.LambdaIntegration(handler));
+
+    // Frontend 
+    //Get The Hosted Zone
+    const zone = route53.HostedZone.fromLookup(this, 'Zone', {
+      domainName: web_app_domain,
+    });
+
+    //Create S3 Bucket for our website
+    const siteBucket = new s3.Bucket(this, 'SiteBucket', {
+        bucketName: web_app_domain,
+        websiteIndexDocument: 'index.html',
+        publicReadAccess: true,
+        removalPolicy: cdk.RemovalPolicy.DESTROY
+    });
+
+    //Create Certificate
+    const siteCertificate = new acm.DnsValidatedCertificate(this, 'SiteCertificate', {
+        domainName: web_app_domain,
+        hostedZone: zone,
+        region: 'us-east-1'  //standard for acm certs
+    });
+
+
+    //Create CloudFront Distribution
+    const siteDistribution = new cloudfront.CloudFrontWebDistribution(this, 'SiteDistribution', {
+        viewerCertificate: cloudfront.ViewerCertificate.fromAcmCertificate(siteCertificate, {
+            securityPolicy: cloudfront.SecurityPolicyProtocol.TLS_V1_2_2021,
+            aliases: [web_app_domain],
+            sslMethod: cloudfront.SSLMethod.SNI
+        }),
+        originConfigs: [{
+            customOriginSource: {
+                domainName: siteBucket.bucketWebsiteDomainName,
+                originProtocolPolicy: cloudfront.OriginProtocolPolicy.HTTP_ONLY
+            },
+            behaviors: [{
+                isDefaultBehavior: true
+            }]
+        }]
+    });
+
+    //Create A Record Custom Domain to CloudFront CDN
+    new route53.ARecord(this, "SiteRecord", {
+      recordName: web_app_domain,
+      target: route53.RecordTarget.fromAlias(new targets.CloudFrontTarget(siteDistribution)),
+      zone
+  });
+
+    // Deploy code to s3
+    new s3deploy.BucketDeployment(this, 'S3FrontendDeployment', {
+      sources: [s3deploy.Source.asset('..\\frontend\\build')],
+      destinationBucket: siteBucket,
+      distribution: siteDistribution,
+      distributionPaths: ["/*"]
+    });
 
   }
 }
