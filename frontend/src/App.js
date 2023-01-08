88e4d4693380fa722238e33ec20ad2405b4086c3
Jackson Kennedy <jtk5aw@virginia.edu>
2023-01-08 18:00:18 -0500

    Get frontend into deployable state
    Start including .js files in the project first.
    Then make changes to have the front-end call the Api Gateway,
    and then render the image.

---
+++ frontend/src/App.js
@@ -0,0 +1,45 @@
+import heart from './HUMAN_HEART.svg';
+import { TODAYS_IMAGE_ENDPOINT } from './config/api';
+import './App.css';
+
+import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  const [base64ImageString, setBase64ImageString] = useState('');
+  const [showImage, setShowImage] = useState(false);
+
+  useEffect(() => {
+    axios.get(TODAYS_IMAGE_ENDPOINT)
+    .then(res => {
+        const returnedString = res.data;
+        setBase64ImageString(returnedString);
+      })
+  }, []);
+
+  const onClick = () => setShowImage(true);
+
+  return (
+    <div className="App">
+      <div className="Title-Header" onClick={onClick}> 
+        <img src={heart} className="Header-Image" alt="Human heart" />
+        <p>
+          Click only if you're Maeov. No one else click 😡
+        </p>
+      </div>
+      { showImage ? <AppBody props={{ 'imageString': base64ImageString }} /> : null }
+    </div>
+  );
+}
+
+const AppBody = ({props}) => (
+  <div className="App-Body">
+      <img src={`data:image/jpg;base64,${props.imageString}`} className="Todays-Image" alt="todays pic" />
+      <p className="Todays-Text">
+        Here is todays specially selected image 😎
+        I hope you like this one and I hope you come back tomorrow for another one. 
+      </p>
+  </div>
+);
+
+export default App;
---
+++ frontend/src/config/api.js
@@ -0,0 +1,3 @@
+const BASE_API = 'https://0ryoon9c4d.execute-api.us-west-1.amazonaws.com/prod';
+
+export const TODAYS_IMAGE_ENDPOINT = BASE_API + '/todays-image';
